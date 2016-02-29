397ca757f94aa8048c6f25fd85d06140ccc5c6ce
thomas-evans <tce003@gmail.com>
2016-02-28 20:26:09 -0500

    Refactoring and directory categories
    Abstracted data manipulation out of the controllers and into factories.
    Added a capitalization filter.

--- directory/controller/directoryCTRL.js
+++ directory/controller/directoryCTRL.js
@@ -1,40 +1,22 @@
 (function(){
-    angular.module('directory').controller('DirectoryCTRL', ['directoryDataFactory', function(directoryDataFactory){
+    angular.module('directory').controller('DirectoryCTRL', ['directoryCatagoriesFactory', 'directoryDataFactory', function(directoryCatagoriesFactory, directoryDataFactory){
         var self = this;
         self.directoryData = {};
         self.categoryData = [];
         self.displayData = {};
         self.categoryStatus = true;
         self.filterStatus = false;
-        function getData(){
-            return directoryDataFactory.getDirectoryData()
-            .then(function(data) {
-                self.directoryData = data;
-                getCategories(self.directoryData);
-                console.log(self.categoryData);
-                return self.directoryData;
+        self.getData = getData;
+        function getCategories(){
+            return directoryCatagoriesFactory.getCategories().then(function(data){
+                self.categoryData = data;
             });
         }
-        function uniq(a){
-            var seen = {},
-                out = [],
-                len = a.length,
-                j = 0;
-            for(var i = 0; i < len; i++){
-                var item = a[i];
-                if(seen[item] !== 1){
-                    seen[item] = 1;
-                    out[j++] = item;
-                }
-            }
-            return out;
-        }
-        function getCategories(data){
-            angular.forEach(data, function(value, key){
-                self.categoryData.push(value.type);
+        getCategories();
+        function getData(type){
+            return directoryDataFactory.getDirectoryData(type).then(function(data){
+                self.displayData = data;
             });
-            self.categoryData = uniq(self.categoryData);
         }
-        getData();
     }]);
 }());
